3958f1eb9d1dfadd0bc9277060b29189c81cd1aa
jorge-ros <120482252+jorge-ros@users.noreply.github.com>
2023-01-25 08:37:13 -0600

    added difficulty variable

--- main.ts
+++ main.ts
@@ -121,3 +121,4 @@ scene.setBackgroundImage(img`
     ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
     ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
     `)
+let difficulty = game.askForString("Choose either \"easy\" or \"hard\"", 4)
